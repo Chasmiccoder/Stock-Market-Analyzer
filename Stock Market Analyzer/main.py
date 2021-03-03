dea10103681b0fcb5cdb211499dc60c4540c8b31
Chasmic Coder <chasmiccoder@gmail.com>
2021-03-03 17:13:24 +0530

    Started Web Scraping

--- Stock Market Analyzer/main.py
+++ Stock Market Analyzer/main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+# Custom module which extracts relevant articles
+import newsWebScraper
+
+keywords = [ "Google" ]
+news = newsWebScraper.collect_news( keywords )
+
+print("News Collected:", news )
---
+++ Stock Market Analyzer/newsWebScraper.py
@@ -0,0 +1,15 @@
+def collect_news( keywords ):
+    """
+    Performs Web Scraping on various websites which involve the keywords
+
+    Params
+    ======
+    keywords (list of strings) = consists of the keywords to be used while searching the web
+
+    Returns
+    =======
+    news (list of strings) = Text scraped from news articles with relevant keywords
+    """
+    
+    news = "Hello"
+    return news
